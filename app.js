0ef070116df8a786461256fa7a72fd21f0f4e187
JonyBu <59262785+JonyBu@users.noreply.github.com>
2021-05-25 00:32:53 -0300

    deploy fix

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
 
+
 // Middlewares
 const { handlerNotFound, unless } = require('./ServerDB/middlewares');
 const { upload } = require('./ServerDB/middlewares/multer');
@@ -15,6 +16,32 @@ const app = express();
 app.use(cors());
 app.use(logger('dev', { skip: () => process.env.NODE_ENV === 'test' }));
 app.use(express.json());
+
+//HEROKU
+const path = require('path');
+const bodyParser = require("body-parser");
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+
+app.use(cors())
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'cliente/build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'Client/build', 'index.html'));
+});
+
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("cliente/build"));
+  
+	app.get("*", (req, res) => {
+	  res.sendFile(path.resolve(__dirname, "cliente", "build", "index.html"));
+	});
+  }
 
 /* IMAGES MIDDLEWARES */
 app.use(unless('/organizations/1/public/1', upload));
@@ -33,5 +60,6 @@ app.use(function (err, req, res, next) {
 	// render the error page
 	res.status(err.status || 500);
 });
+
 
 module.exports = app;
